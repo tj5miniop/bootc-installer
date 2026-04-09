873cda58acbcf9622a7c61e7d3812333e6b16ea8
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2026-04-09 10:28:50 +0000

    install and enable NetworkManager for WIFI

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,14 +3,13 @@
 set -ouex pipefail
 
 ### Install packages
+# install core networkmanager
+dnf5 install -y NetworkManager systemd
+systemctl enable Networkmanager
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+
+
 
 # Use a COPR Example:
 #
